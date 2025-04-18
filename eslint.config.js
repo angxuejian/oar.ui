4a2890b9ecdd9d160c5f9bf503afb0c2d04edd86
angxuejian <xuejian.ang@gmail.com>
2025-04-18 09:14:16 +0800

    fix:  fixed “ [@vue/compiler-sfc] `defineProps` is a compiler macro and no longer needs to be imported”

--- eslint.config.js
+++ eslint.config.js
@@ -26,7 +26,8 @@ export default [
   {
     rules: {
       'vue/multi-word-component-names': 'off',
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "vue/setup-compiler-macros": true
     },
   },
 ]
