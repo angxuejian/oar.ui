28a7bc8cdacfeb56fe3f2a1c3b2546fe3fcbd364
angxuejian <xuejian.ang@gmail.com>
2025-12-04 16:38:54 +0800

    chore: update build package config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vitePluginVueMarkdown from './plugins/vite-plugin-vue-markdown'
+
+// import { visualizer } from 'rollup-plugin-visualizer'
 // import vueJsx from '@vitejs/plugin-vue-jsx'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -16,6 +18,13 @@ export default defineConfig({
     vue({include: [/\.vue$/, /\.md$/] }),
     vitePluginVueMarkdown(),
 
+    // visualizer({
+    //   filename: 'dist/stats.html', // 输出分析文件路径
+    //   open: true,                  // 构建后自动打开浏览器查看
+    //   gzipSize: true,              // 显示 gzip 压缩体积
+    //   brotliSize: true,            // 显示 brotli 压缩体积
+    //   template: 'treemap',         // 可选: sunburst / treemap / network
+    // }),
     // vueJsx(),
     // vueDevTools(),
   ],
@@ -34,6 +43,35 @@ export default defineConfig({
       scss: {
         additionalData: `@use "${themeBase}" as *;`,
         api: 'modern-compiler',
+      }
+    }
+  },
+
+  // 预构建，用于 Vite dev server 启动更快、HMR 更快。
+  optimizeDeps: {
+    include: ['vue', 'vue-router', '@vueuse/core']
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild',
+
+    rollupOptions: {
+      // external: ['vue'], // 打包组件库时，将vue剔除
+      treeshake: true,
+      output: {
+        entryFileNames: 'assets/[name].[hash].js', // 入口文件（main.ts / index.ts）产物的命名规则
+        chunkFileNames: 'assets/js/[name].[hash].js', // 非入口的代码分块（动态导入的 chunk）产物的命名规则
+        assetFileNames: 'assets/[ext]/[name].[hash].[ext]', // 静态资源（图片、字体、css 等）产物的命名规则
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes('@vueuse')) return 'vueuse';
+            if (id.includes('throttle-debounce')) return 'throttle-debounce';
+            if (id.includes('lucide-vue-next')) return 'vicons';
+            return 'vendor'
+          }
+        }
       }
     }
   }
